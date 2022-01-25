c2eb547854a04c735ee9dfea252a888d9655a0df
Daniel Murphy <73913084+dmquinn@users.noreply.github.com>
2022-01-25 14:10:53 +0100

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: ["avatars.githubusercontent.com"],
-  },
-  env: {
-    TOKEN: "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",
-  },
-};
